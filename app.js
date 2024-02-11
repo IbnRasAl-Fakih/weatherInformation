2abb69e40eb43d7bd893f4a12bbfc7d4cc245bb7
IbnRasAl-Fakih <rasul.eletai@gmail.com>
2024-02-12 04:35:56 +0600

    ninth release

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require('path');
 require('dotenv').config();
 const app = express();
 const POST = 3000;
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-app.use(express.static("public"));
+app.use('/public', express.static(path.join(process.cwd(), 'public')));
 app.set("view engine", "ejs");
+app.set('views',path.join(process.cwd(), 'views'));
 
 mongoose.connect(process.env.MONGO_URL).then( () => {
     console.log("Connected to db");
