55a91308fcfd3ee5d874c6f7cac18eeed07802d1
IbnRasAl-Fakih <rasul.eletai@gmail.com>
2024-02-12 04:42:19 +0600

    tenth release

--- public/JS/location.js
+++ public/JS/location.js
@@ -1,15 +1,15 @@
 function home() {
     const urlParams = new URLSearchParams(window.location.search);
     const user = urlParams.get('username');
-    location.href = `http://localhost:3000/weather?username=${user}`;
+    location.href = `/weather?username=${user}`;
 }
 
 function history() {
     const urlParams = new URLSearchParams(window.location.search);
     const user = urlParams.get('username');
-    location.href = `http://localhost:3000/history?username=${user}`;
+    location.href = `/history?username=${user}`;
 }
 
 function logOut() {
-    location.href = `http://localhost:3000`;
+    location.href = `/`;
 }
